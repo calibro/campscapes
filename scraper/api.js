2cd0d0917530c710305f52237bbb62c553f32c28
Mauro Bianchi <bianchimro@gmail.com>
2019-08-26 00:03:16 +0200

    extending scraper

--- scraper/api.js
+++ scraper/api.js
@@ -2,12 +2,137 @@ const BASE_URL = 'http://www.dbportal.ic-access.specs-lab.com/api'
 const API_KEY = 'dd9451bfd7f887f996e82d88677336abd9c910ab'
 const request = require('superagent')
 const queryString = require('query-string');
+const get = require('lodash/get')
 // `http://www.dbportal.ic-access.specs-lab.com/api/items?key=dd9451bfd7f887f996e82d88677336abd9c910ab`
 
+
+
+const FIELDS_TO_TYPES = {
+  'latitude': Number,
+  'longitude': Number,
+  // ...
+}
+
+
+async function simplifyItem(item){
+  const elementTexts = get(item, 'element_texts', [])
+
+  let data = {}
+
+  elementTexts.forEach(elementText => {
+    const name = elementText.element.name.toLowerCase()
+    const mapping  = get(FIELDS_TO_TYPES, name, x => x)
+    data[name] =  mapping(elementText.text)
+  });
+
+  let files
+  if(item.files){
+    data.files = await getUrlFromApiResponse(item.files.url)
+  }
+
+  return {
+    ...item,
+    element_texts: undefined,
+    item_type: get(item, 'item_type.name'),
+    data
+  }
+}
+
+
+
+async function enrichWithRelations(item, relations){
+  // TODO: understand if we should use this, object_item_id or both
+  const subjectRelations = relations.filter(relation => relation.subject_item_id === item.id)
+  const objectRelations = relations.filter(relation => relation.object_item_id === item.id)
+  
+  const objects = await Promise.all(objectRelations.map(async function(relation){
+    return await getItem(relation.subject_item_id)
+  }))
+  
+  const subjects = await Promise.all(subjectRelations.map(async function(relation){
+    return await getItem(relation.object_item_id)
+  }))
+  
+  return {
+    ...item,
+    relations: {
+      objects, subjects
+    }
+    
+  }
+}
+
+
+function greedyAPIMaker(apFn){
+
+  return async function innerCallable(options = {
+    query: {},
+    greedy: true,
+    relations: [],
+  }) {
+    let out = []
+    const query = options.query || {}
+    const page = query.page || 1
+    try {
+      const pageData = await apFn(options.query)
+      const lastUrlAndQuery = queryString.parseUrl(pageData.links.last)
+      const lastPage = lastUrlAndQuery.query.page
+      let itemsList = await Promise.all(pageData.body.map(simplifyItem))
+      if(options.relations && options.relations.length) {
+        itemsList= await Promise.all(
+          itemsList.map(async function(item){
+            return await enrichWithRelations(item, options.relations)
+          })
+        )
+      }
+      out = out.concat(itemsList)
+  
+      if(options.greedy && page < lastPage){
+        const nextPage = await innerCallable({ ...options.query, page: page+1})
+        out = out.concat(nextPage)
+      }
+  
+    } catch (err) {
+      throw(err)
+    }
+    
+    return out
+    
+  }
+}
+
+
+async function getUrlFromApiResponse(url) {
+  const urlAndQuery = queryString.parseUrl(url)
+  try {
+    var response = await request
+    .get(urlAndQuery.url)
+    .query({...urlAndQuery.query, key: API_KEY})
+    .then(({body}) => body)
+  } catch (err) {
+    throw(err)
+  }
+  return response
+}
+
+
+async function getItem(id) {
+  try {
+    var response = await request
+    .get(`${BASE_URL}/items/${id}`)
+    .query({key: API_KEY})
+    .then(async function(resp){
+      return await simplifyItem(resp.body)
+    })
+  //  
+  } catch (err) {
+    // do something with err...
+  }
+  return response
+}
  
 
 async function getItems(q={}) {
-  console.log("q", q)
   try {
     var response = await request
     .get(`${BASE_URL}/items`)
@@ -19,32 +144,37 @@ async function getItems(q={}) {
   return response
 }
 
-async function getItemsRecursive(q={}) {
-  let out = []
-  const page = q.page || 1
-
+async function getTags(q={}) {
   try {
-    const pageData = await getItems(q)
-    const lastUrlAndQuery = queryString.parseUrl(pageData.links.last)
-    const lastPage = lastUrlAndQuery.query.page
-    out = out.concat(pageData.body)
-
-    if(page < lastPage){
-      const nextPage = await getItemsRecursive({ ...q, page: page+1})
-      out = out.concat(nextPage)
-    }
-
+    var response = await request
+    .get(`${BASE_URL}/tags`)
+    .query({key: API_KEY, ...q})
+    .then(({body}) => body)
+  //  
   } catch (err) {
-    throw(err)
+    // do something with err...
   }
-  
-  return out
-  
+  return response
+}
+
+async function getItemRelations(q={}) {
+  try {
+    var response = await request
+    .get(`${BASE_URL}/itemrelations`)
+    .query({key: API_KEY, ...q})
+    .then(({body}) => body)
+  //  
+  } catch (err) {
+    // do something with err...
+  }
+  return response
 }
 
 
 module.exports.getItems = getItems
-module.exports.getItemsRecursive = getItemsRecursive
+module.exports.getItemsGreedy = greedyAPIMaker(getItems)
+module.exports.getTags = getTags
+module.exports.getItemRelations = getItemRelations
 
 
 
--- scraper/update.js
+++ scraper/update.js
@@ -27,10 +27,64 @@ async function main(options){
     fs.mkdirSync(targetDir)
   }
 
-  //example: getting camps
-  const data = await api.getItemsRecursive({item_type: 32})
+
+  //getting relations
+  console.log(colors.yellow(`Getting relations`))
+  const relations = await api.getItemRelations()
+  const relationsFilename = path.join(targetDir, 'relations.json')
+  fs.writeFileSync(relationsFilename, JSON.stringify(relations))
+    
+  //getting themes (tags)
+  console.log(colors.yellow(`Getting themes`))
+  const themes = await api.getTags()
+  const themesFilename = path.join(targetDir, 'themes.json')
+  fs.writeFileSync(themesFilename, JSON.stringify(themes))
+
+  //getting camps
+  console.log(colors.yellow(`Getting camps`))
+  const camps = await api.getItemsGreedy({ 
+    query: {item_type: 32},
+    relations
+  })
   const campsFilename = path.join(targetDir, 'camps.json')
-  fs.writeFileSync(campsFilename, JSON.stringify(data))
+  fs.writeFileSync(campsFilename, JSON.stringify(camps))
+
+  //getting icons
+  console.log(colors.yellow(`Getting icons`))
+  const icons = await api.getItemsGreedy({ 
+    query: {item_type: 34},
+    relations
+  })
+  const iconsFilename = path.join(targetDir, 'icons.json')
+  fs.writeFileSync(iconsFilename, JSON.stringify(icons))
+
+
+  // //getting hyperlinks
+  // console.log(colors.yellow(`Getting hyperlinks`))
+  // const hyperlinks = await api.getItemsGreedy({ 
+  //   query: {item_type: 11},
+  //   relations
+  // })
+  // const hyperlinksFilename = path.join(targetDir, 'hyperlinks.json')
+  // fs.writeFileSync(hyperlinksFilename, JSON.stringify(hyperlinks))
+
+  // //getting resources
+  // console.log(colors.yellow(`Getting resourcess`))
+  // const resources = await api.getItemsGreedy({ 
+  //   query: {item_type: 33},
+  //   relations
+  // })
+  // const resourcesFilename = path.join(targetDir, 'resources.json')
+  // fs.writeFileSync(resourcesFilename, JSON.stringify(resources))
+
+  // //getting references
+  // console.log(colors.yellow(`Getting references`))
+  // const references = await api.getItemsGreedy({ 
+  //   query: {item_type: 33},
+  //   relations
+  // })
+  // const referencesFilename = path.join(targetDir, 'references.json')
+  // fs.writeFileSync(referencesFilename, JSON.stringify(references))
 
 
 }
