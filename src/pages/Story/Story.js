53cc17dca3a017c8c2c5b9a1905cfe6dd020cf76
Mauro Bianchi <bianchimro@gmail.com>
2019-08-28 18:05:53 +0200

    fixing story page

--- src/pages/Story/Story.js
+++ src/pages/Story/Story.js
@@ -2,8 +2,10 @@ import React, { useContext, useMemo } from "react";
 import { StoriesContext } from "../../dataProviders";
 import find from "lodash/find";
 
-const Story = () => {
+const Story = ({ match }) => {
   const stories = useContext(StoriesContext);
+  const { params } = match;
+
   const story = useMemo(() => {
     return find(stories, item => item.data.slug === params.slug);
   }, [stories, params.slug]);
