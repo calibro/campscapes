f90a1950cecf166bf47f7301714344279baaf15e
uf0 <scnepa222@gmail.com>
2019-09-12 19:12:38 +0200

    bootstrap map

---
+++ src/components/CampsMap/CampsMap.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import { point, featureCollection } from "@turf/helpers";
+import Bbox from "@turf/bbox";
+import styles from "./CampsMap.module.scss";
+
+const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+const Map = ReactMapboxGl({
+  accessToken: ACCESS_TOKEN,
+  maxZoom: 7,
+  minZoom: 2
+});
+
+const CampsMap = ({ camps }) => {
+  const features = camps.map(camp =>
+    point([camp.data.longitude, camp.data.latitude], {
+      id: camp.id,
+      ...camp.data
+    })
+  );
+  const geojson = featureCollection(features);
+  const bbox = Bbox(geojson);
+
+  console.log(camps, features);
+
+  return (
+    <div className={styles.mapContainer}>
+      <Map
+        style="mapbox://styles/mapbox/satellite-v9"
+        containerStyle={{
+          height: "100%",
+          width: "100%"
+        }}
+        fitBounds={[[bbox[0], bbox[1]], [bbox[2], bbox[3]]]}
+      >
+        {features.map(feature => {
+          return (
+            <Marker
+              key={feature.properties.id}
+              coordinates={feature.geometry.coordinates}
+              anchor="center"
+              offset={[0, 20]}
+            >
+              <div className="text-center">
+                <svg width="31" height="31">
+                  <circle cx="50%" cy="50%" fill="white" r="5"></circle>
+                  <circle
+                    cx="50%"
+                    cy="50%"
+                    fill="none"
+                    r="15"
+                    stroke="white"
+                  ></circle>
+                </svg>
+                <p className="mt-2 mb-0">{feature.properties.title}</p>
+              </div>
+            </Marker>
+          );
+        })}
+      </Map>
+    </div>
+  );
+};
+
+export default CampsMap;
--- src/pages/Camps/Camps.js
+++ src/pages/Camps/Camps.js
@@ -2,20 +2,30 @@ import React, { useContext } from "react";
 import { CampsContext } from "../../dataProviders";
 import { Link } from "react-router-dom";
 import Menu from "../../components/Menu";
+import CampsMap from "../../components/CampsMap";
+import styles from "./Camps.module.scss";
 
 const Camps = () => {
   const camps = useContext(CampsContext);
-  console.log("camps data", camps);
 
   return (
-    <div>
-      <h2>Camps page</h2>
-      {camps.map(camp => (
-        <p key={camp.id}>
-          <Link to={`/camps/${camp.data.siteName}`}>{camp.data.siteName}</Link>
-        </p>
-      ))}
-      <Menu></Menu>
+    <div className={styles.campsContainer}>
+      {camps.length > 0 && <CampsMap camps={camps}></CampsMap>}
+      <div className="container">
+        <div className="row align-items-baseline">
+          <div className="col-12 col-md-3">
+            <h1>Camps</h1>
+          </div>
+          <div className="col-12 col-md-9">
+            <p>
+              Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Vivamus sagittis
+              lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis
+              consectetur purus sit amet fermentum.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
