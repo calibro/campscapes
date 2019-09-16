ea7fe2ad9eecb8395d412eefbeac273b4af943d8
uf0 <scnepa222@gmail.com>
2019-09-16 17:43:09 +0200

    wip homepage

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,12 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MdArrowForward } from "react-icons/md";
+import { Text } from "@vx/text";
+import styles from "./Intro.module.scss";
 
 const Intro = () => {
   return (
-    <div>
-      <h2>Intro page</h2>
-      <div>
-        <Link to="/home">Home</Link>
+    <div className="w-100 h-100 position-relative">
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <div>
+            <svg className={styles.svg}>
+              <text
+                textAnchor="middle"
+                x="50%"
+                y="50%"
+                className={styles.title}
+              >
+                Accessing Campscapes
+              </text>
+              {/*<Text
+                verticalAnchor="start"
+                className={styles.title}
+                fontSize="5rem"
+                fontFamily="'CeraStencilPRO', sans-serif;"
+              >
+                Accessing Campscapes
+              </Text>*/}
+            </svg>
+          </div>
+          <div className={styles.nextButton}>
+            <MdArrowForward color="white" size="1.5rem"></MdArrowForward>
+          </div>
+        </div>
+        <div className={styles.skip}>
+          <Link to="/home">Skip</Link>
+        </div>
       </div>
     </div>
   );
