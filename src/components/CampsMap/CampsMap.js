5cf3bee63df6d08e7f7d955d883e238198d94bfa
uf0 <scnepa222@gmail.com>
2019-10-21 16:55:44 +0200

    move form marker to layer

--- src/components/CampsMap/CampsMap.js
+++ src/components/CampsMap/CampsMap.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import ReactMapboxGl, { Marker, GeoJSONLayer } from "react-mapbox-gl";
 import { point, featureCollection } from "@turf/helpers";
 import Bbox from "@turf/bbox";
 import styles from "./CampsMap.module.scss";
@@ -12,7 +12,8 @@ const Map = ReactMapboxGl({
   minZoom: 2
 });
 
-const CampsMap = ({ camps }) => {
+const CampsMap = ({ camps, history }) => {
+  let hover = null;
   const features = camps.map(camp =>
     point([camp.data.longitude, camp.data.latitude], {
       id: camp.id,
@@ -22,7 +23,37 @@ const CampsMap = ({ camps }) => {
   const geojson = featureCollection(features);
   const bbox = Bbox(geojson);
 
-  console.log(camps, features);
+  const geojsonOnMouseMove = (cursor, map, features) => {
+    map.getCanvas().style.cursor = cursor;
+    if (features.length > 0) {
+      if (hover) {
+        map.setFeatureState(
+          { source: features[0].source, id: hover.id },
+          { hover: false }
+        );
+      }
+      hover = features[0];
+      map.setFeatureState(
+        { source: features[0].source, id: features[0].id },
+        { hover: true }
+      );
+    }
+  };
+
+  const geojsonOnMouseLeave = (cursor, map) => {
+    map.getCanvas().style.cursor = cursor;
+    if (hover) {
+      map.setFeatureState(
+        { source: hover.source, id: hover.id },
+        { hover: false }
+      );
+    }
+    hover = null;
+  };
+
+  const geojsonOnClick = camp => {
+    history.push(`/camps/${camp}`);
+  };
 
   return (
     <div className={styles.mapContainer}>
@@ -35,43 +66,64 @@ const CampsMap = ({ camps }) => {
         fitBounds={[[bbox[0], bbox[1]], [bbox[2], bbox[3]]]}
         fitBoundsOptions={{ padding: 100 }}
       >
-        {features.map(feature => {
-          return (
-            <Marker
-              key={feature.properties.id}
-              coordinates={feature.geometry.coordinates}
-              anchor="center"
-              offset={[0, 20]}
-            >
-              <div className={styles.marker}>
-                <svg width="31" height="31">
-                  <circle
-                    cx="50%"
-                    cy="50%"
-                    fill="white"
-                    r="5"
-                    className={styles.innerCircle}
-                  ></circle>
-                  <circle
-                    cx="50%"
-                    cy="50%"
-                    fill="none"
-                    r="15"
-                    stroke="white"
-                  ></circle>
-                </svg>
-                <p className="mt-1 mb-0">
-                  <Link to={`/camps/${feature.properties.siteName}`}>
-                    {feature.properties.title}
-                  </Link>
-                </p>
-              </div>
-            </Marker>
-          );
-        })}
+        <GeoJSONLayer
+          data={geojson}
+          circlePaint={{
+            "circle-color": "transparent",
+            "circle-stroke-width": 1,
+            "circle-radius": 10,
+            "circle-stroke-color": "white"
+          }}
+        />
+        <GeoJSONLayer
+          data={geojson}
+          symbolPaint={{
+            "text-color": "white",
+            "text-halo-width": 1,
+            "text-halo-color": "black"
+          }}
+          circlePaint={{
+            "circle-color": "white",
+            "circle-radius": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              8,
+              5
+            ]
+          }}
+          symbolLayout={{
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+          }}
+          circleOnMouseMove={e => {
+            geojsonOnMouseMove("pointer", e.target, e.features);
+          }}
+          circleOnMouseLeave={e => {
+            geojsonOnMouseLeave("", e.target);
+          }}
+          circleOnClick={e => {
+            if (e.features.length > 0) {
+              geojsonOnClick(e.features[0].properties.siteName);
+            }
+          }}
+          symbolOnMouseMove={e => {
+            geojsonOnMouseMove("pointer", e.target, e.features);
+          }}
+          symbolOnMouseLeave={e => {
+            geojsonOnMouseLeave("", e.target);
+          }}
+          symbolOnClick={e => {
+            if (e.features.length > 0) {
+              geojsonOnClick(e.features[0].properties.siteName);
+            }
+          }}
+          sourceOptions={{ generateId: true }}
+        />
       </Map>
     </div>
   );
 };
 
-export default CampsMap;
+export default withRouter(CampsMap);
