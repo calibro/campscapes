171fc992dcd67c012314f924325f03ecd5e813c6
uf0 <scnepa222@gmail.com>
2019-11-18 11:27:38 +0100

    wip network

--- src/components/CampNetwork/CampNetwork.js
+++ src/components/CampNetwork/CampNetwork.js
@@ -1,17 +1,41 @@
-import React, { useContext, useMemo, useState, useEffect, useRef } from "react";
-import useDimensions from "react-use-dimensions";
-import { Link } from "react-router-dom";
-import find from "lodash/find";
-import omit from "lodash/omit";
+import React, { useMemo, useState } from "react";
 import groupBy from "lodash/groupBy";
-import cloneDeep from "lodash/cloneDeep";
-import get from "lodash/get";
-import { scaleTime, scaleLinear } from "d3-scale";
+import sortBy from "lodash/sortBy";
+import { scaleSqrt } from "d3-scale";
 import { min, max } from "d3-array";
-import { Graph } from "react-d3-graph";
+import {
+  ForceGraph,
+  InteractiveForceGraph,
+  ForceGraphNode,
+  ForceGraphLink
+} from "react-vis-force";
+import { ZoomContainer } from "reactochart";
+import NodeRenderer from "./NodeRenderer";
 import styles from "./CampNetwork.module.scss";
 
-const CampNetwork = ({ annotatedGraph, width, height }) => {
+const CampNetworkUber = ({ annotatedGraph, width, height }) => {
+  const [nodeHover, setNodeHover] = useState(null);
+
+  const setOpacityLink = link => {
+    const neighbors = nodeHover.neighbors.map(d => {
+      if (nodeHover.data.itemType === "story") {
+        return nodeHover.id + "_node_" + d;
+      } else {
+        return "node_" + d + "_" + nodeHover.id;
+      }
+    });
+    return neighbors.includes(link.source + "_" + link.target) ? 1 : 0;
+  };
+
+  const setOpacityNode = node => {
+    const neighbors = nodeHover.neighbors.map(d => "node_" + d);
+    return node.id === nodeHover.id
+      ? 1
+      : neighbors.includes(node.id)
+      ? 1
+      : 0.25;
+  };
+
   const nodeScale = useMemo(() => {
     if (!annotatedGraph.nodes) {
       return;
@@ -22,9 +46,9 @@ const CampNetwork = ({ annotatedGraph, width, height }) => {
     const minDegree = min(degrees);
     const maxDegree = max(degrees);
 
-    return scaleLinear()
+    return scaleSqrt()
       .domain([minDegree, maxDegree])
-      .range([400, 1000]);
+      .range([5, 20]);
   }, [annotatedGraph]);
 
   const links = useMemo(() => {
@@ -36,8 +60,8 @@ const CampNetwork = ({ annotatedGraph, width, height }) => {
     return Object.values(bySt).map(k => ({
       //...k[0],
       source: "node_" + k[0].source,
-      target: "node_" + k[0].target
-      //value: k.length
+      target: "node_" + k[0].target,
+      value: k.length
     }));
   }, [annotatedGraph.links]);
 
@@ -45,141 +69,80 @@ const CampNetwork = ({ annotatedGraph, width, height }) => {
     if (!nodeScale) {
       return null;
     }
-    return annotatedGraph.nodes.map(node => ({
-      //...node,
-      id: "node_" + node.id,
-      size: node.inDegree ? nodeScale(node.inDegree) : 200
-    }));
-  }, [annotatedGraph.nodes, nodeScale]);
 
-  const graphConfig = useMemo(() => {
-    if (!width || !height) {
-      return null;
-    }
-    return {
-      directed: true,
-      automaticRearrangeAfterDropNode: false,
-      focusAnimationDuration: 0.2,
-      linkHighlightBehavior: true,
-      nodeHighlightBehavior: true,
-      highlightOpacity: 0.5,
-      width: width,
-      height: height,
-      d3: {
-        gravity: -250,
-        linkLength: 120
-      },
-      node: {
-        highlightColor: "#ff6600",
-        highlightStrokeColor: "#ff6600",
-        highlightFontWeight: "bold",
-        fontSize: 14,
-        highlightFontSize: 14
-      },
-      link: {
-        highlightColor: "#ff6600",
-        strokeWidth: 3
-      }
-    };
-  }, [width, height]);
+    const formattedNodes = annotatedGraph.nodes.map(node => ({
+      data: node.data,
+      neighbors: node.neighbors,
+      id: "node_" + node.id,
+      radius: node.inDegree ? nodeScale(node.inDegree) : 20
+    }));
+    // FIXME: sorting nodes: story should be the latest
+    return sortBy(formattedNodes, [d => d.data.itemType]);
+  }, [annotatedGraph.nodes, nodeScale]);
 
   //console.log(nodes, links, width, height);
   return (
     nodes &&
     links &&
-    graphConfig &&
     height &&
     width && (
-      <Graph
-        id="ciao"
-        data={{ nodes: nodes, links: links }}
-        config={graphConfig}
-      />
+      <ZoomContainer width={width} height={height}>
+        <ForceGraph
+          simulationOptions={{
+            strength: {
+              charge: -150
+            },
+            animate: true,
+            height: height,
+            width: width,
+            radiusMargin: 10
+          }}
+        >
+          {/*{nodes.map(node => {
+            return (
+              <CustomNode
+                key={node.id}
+                node={node}
+                fill={nodeHover && nodeHover.id === node.id ? "red" : "white"}
+                onMouseOver={() => {
+                  console.log(node);
+                  setNodeHover(node);
+                }}
+                onMouseOut={() => {
+                  setNodeHover(null);
+                }}
+              />
+            );
+          })}*/}
+          {nodes.map(node => {
+            return (
+              <NodeRenderer
+                key={node.id}
+                node={node}
+                fill={"#101012"}
+                onMouseOver={() => {
+                  setNodeHover(node);
+                }}
+                onMouseOut={() => {
+                  setNodeHover(null);
+                }}
+                opacity={nodeHover ? setOpacityNode(node) : 1}
+              />
+            );
+          })}
+          {links.map(link => {
+            return (
+              <ForceGraphLink
+                key={link.source + link.target}
+                link={link}
+                opacity={nodeHover ? setOpacityLink(link) : 0}
+              />
+            );
+          })}
+        </ForceGraph>
+      </ZoomContainer>
     )
   );
-
-  // const simulation = useRef(null);
-
-  // useEffect(() => {
-  //   if (simulation.current) {
-  //     simulation.current.stop();
-  //   }
-  //   const outGraph = cloneDeep(annotatedGraph);
-  //   if (!outGraph.nodes || !outGraph.links) {
-  //     return;
-  //   }
-
-  //   // Custom force to put all nodes in a box
-  //   function boxingForce() {
-  //     outGraph.nodes.forEach(node => {
-  //       node.x = max([min([node.x, width - 30]), 30]);
-  //       node.y = max([min([node.y, height - 30]), 30]);
-  //     });
-  //   }
-
-  //   simulation.current = forceSimulation(outGraph.nodes)
-  //     .force(
-  //       "charge",
-  //       forceManyBody().strength(-30)
-  //       // .distanceMax(120)
-  //     )
-  //     .force("link", forceLink(outGraph.links).id(d => d.id))
-  //     .force(
-  //       "collide",
-  //       forceCollide(node =>
-  //         get(node, "data.itemType") === "story"
-  //           ? 40
-  //           : nodeScale(node.degree) * 4
-  //       ).iterations(3)
-  //     )
-  //     .force("center", forceCenter(width / 2, height / 2))
-  //     .force("box", boxingForce)
-  //     .on("tick", () => {
-  //       const { links, nodes } = outGraph;
-  //       setNodesAndLinks({ nodes, links });
-  //     });
-  // }, [annotatedGraph, height, nodeScale, width]);
-
-  // return (
-  //   <svg height={height} width={width}>
-  //     {nodesAndLinks.nodes && (
-  //       <>
-  //         {nodesAndLinks.links.map((link, i) => (
-  //           <line
-  //             key={i}
-  //             x1={link.source.x}
-  //             y1={link.source.y}
-  //             x2={link.target.x}
-  //             y2={link.target.y}
-  //             stroke="#fff"
-  //           ></line>
-  //         ))}
-  //         {nodesAndLinks.nodes.map(node => (
-  //           <g key={node.id}>
-  //             <circle
-  //               onMouseEnter={() => {
-  //                 setHoverNode(node.id);
-  //               }}
-  //               cx={node.x}
-  //               cy={node.y}
-  //               r={
-  //                 get(node, "data.itemType") === "story"
-  //                   ? "4"
-  //                   : nodeScale(node.degree)
-  //               }
-  //               style={{
-  //                 fill: node.data.itemType === "story" ? "#c82727" : "#fff"
-  //               }}
-  //             ></circle>
-  //             {/* <text fill="#fff" x={node.x} y={node.y}>
-  //           {node.id}
-  //         </text> */}
-  //           </g>
-  //         ))}
-  //       </>
-  //     )}
-  //   </svg>
-  // );
 };
 
-export default CampNetwork;
+export default CampNetworkUber;
---
+++ src/components/CampNetwork/NodeRenderer.js
@@ -0,0 +1,151 @@
+import React from "react";
+import {
+  MdAdd,
+  MdLink,
+  MdLibraryBooks,
+  MdImage,
+  MdPictureAsPdf,
+  MdAudiotrack,
+  MdVideoLibrary,
+  MdNote
+} from "react-icons/md";
+import { Link } from "react-router-dom";
+import { Text } from "@vx/text";
+
+const CustomNodeResource = props => {
+  const { node, className, cx, cy, fill, ...spreadable } = props;
+  const { radius = 5 } = node;
+  return (
+    <React.Fragment>
+      <defs>
+        <pattern
+          id={node.id}
+          width="100%"
+          height="100%"
+          patternContentUnits="objectBoundingBox"
+        >
+          <image
+            href={
+              "http://www.dbportal.ic-access.specs-lab.com/files/square_thumbnails/7d11cb102441828236d52b34c5cc5b66.jpg"
+            }
+            preserveAspectRatio="xMidYMin slice"
+            width="1"
+            height="1"
+          ></image>
+        </pattern>
+      </defs>
+      <circle
+        className={`rv-force__node ${className}`}
+        cx={cx}
+        cy={cy}
+        r={radius}
+        fill={`url(#${node.id})`}
+        {...spreadable}
+      />
+    </React.Fragment>
+  );
+};
+
+const CustomNodeStory = props => {
+  const { node, className, cx, cy, fill, ...spreadable } = props;
+  const { radius = 5 } = node;
+  const translateX = cx ? cx - radius : 0;
+  const translateY = cy ? cy - radius : 0;
+  return (
+    <g
+      {...spreadable}
+      className={`rv-force__node ${className}`}
+      transform={`translate(${translateX},${translateY})`}
+    >
+      <MdAdd
+        size={radius * 2 + "px"}
+        style={{ color: "var(--red-cs)" }}
+      ></MdAdd>
+      <Link to={`/stories/${node.data.slug}`}>
+        <Text
+          style={{ fill: "white" }}
+          width={200}
+          x={radius + radius / 2 + 5}
+          y={radius}
+          verticalAnchor="middle"
+        >
+          {node.data.title}
+        </Text>
+      </Link>
+    </g>
+  );
+};
+
+const CustomNodeOther = props => {
+  const { node, className, cx, cy, fill, ...spreadable } = props;
+  const { radius = 5 } = node;
+  const translateX = cx ? cx - radius : 0;
+  const translateY = cy ? cy - radius : 0;
+  const iconSize = radius * 1.5;
+
+  const switchIcon = type => {
+    switch (type) {
+      case "reference":
+        return (
+          <MdLibraryBooks
+            size={radius * 1.5 + "px"}
+            style={{ color: "var(--red-cs)" }}
+            x={radius - (radius * 1.5) / 2}
+            y={radius - (radius * 1.5) / 2}
+          ></MdLibraryBooks>
+        );
+      case "hyperlink":
+        return (
+          <g transform={`translate(${radius / 3},${radius / 3})`}>
+            <MdLink
+              size={iconSize + "px"}
+              style={{ color: "var(--red-cs)" }}
+            ></MdLink>
+          </g>
+        );
+      default:
+        return (
+          <MdNote
+            size={radius * 1.5 + "px"}
+            style={{ color: "var(--red-cs)" }}
+            x={radius - (radius * 1.5) / 2}
+            y={radius - (radius * 1.5) / 2}
+          ></MdNote>
+        );
+    }
+  };
+
+  return (
+    <g
+      {...spreadable}
+      className={`rv-force__node ${className}`}
+      transform={`translate(${translateX},${translateY})`}
+    >
+      <circle cx={radius} cy={radius} r={radius} fill={fill} />
+      {switchIcon(node.data.itemType)}
+    </g>
+  );
+};
+
+const NodeRenderer = props => {
+  const type = props.node.data.itemType;
+
+  const renderSwitch = type => {
+    switch (type) {
+      case "story":
+        return <CustomNodeStory {...props}></CustomNodeStory>;
+      case "resource":
+        return <CustomNodeResource {...props}></CustomNodeResource>;
+      case "reference":
+        return <CustomNodeOther {...props}></CustomNodeOther>;
+      case "hyperlink":
+        return <CustomNodeOther {...props}></CustomNodeOther>;
+      default:
+        return <CustomNodeOther {...props}></CustomNodeOther>;
+    }
+  };
+
+  return renderSwitch(type);
+};
+
+export default NodeRenderer;
